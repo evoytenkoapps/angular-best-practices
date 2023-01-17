0573e7f0b96617e5cceb4d2718952866cb14a0f0
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-01-17 11:43:10 +0300

    add proxy

--- examples/src/app/components/http-proxy-example/http-proxy-example.component.ts
+++ examples/src/app/components/http-proxy-example/http-proxy-example.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-http-proxy-example',
   templateUrl: './http-proxy-example.component.html',
   styleUrls: ['./http-proxy-example.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HttpProxyExampleComponent implements OnInit {
-
-  constructor() { }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit(): void {
+    this.httpService.getData();
   }
-
 }
