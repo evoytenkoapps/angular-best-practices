f41832fe88c9f9eee9be5a485a4bce3d03066fc5
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-01-17 11:36:05 +0300

    add proxy

---
+++ examples/src/app/components/http-proxy-example/http-proxy.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpProxyExampleComponent } from './http-proxy-example.component';
+
+@NgModule({
+  declarations: [HttpProxyExampleComponent],
+  imports: [CommonModule],
+})
+export class HttpProxyModule {}
